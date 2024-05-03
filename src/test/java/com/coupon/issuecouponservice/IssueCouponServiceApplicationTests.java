6ffbb8d4428ab891863f6365d4d2fadc57350911
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-03 11:36:32 +0900

    init commit

---
+++ src/test/java/com/coupon/issuecouponservice/IssueCouponServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.issuecouponservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueCouponServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
