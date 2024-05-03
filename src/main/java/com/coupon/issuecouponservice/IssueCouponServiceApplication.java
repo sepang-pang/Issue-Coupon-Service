fd4b8bbf723b200e8b4a35c0233657dacc962e7a
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-03 18:01:19 +0900

    [feature]서버 시간대를 UTC로 설정하는 초기화 메소드 추가 ( #3 )
    - 애플리케이션 시작 시 시간대를 UTC로 설정하는 init 메소드를 추가
    
    - @PostConstruct 어노테이션을 사용하여 애플리케이션의 생명주기에 따라 자동으로 실행되도록 구현

--- src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
+++ src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
@@ -1,13 +1,23 @@
 package com.coupon.issuecouponservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class IssueCouponServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IssueCouponServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
