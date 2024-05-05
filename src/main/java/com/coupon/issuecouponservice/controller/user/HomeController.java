d27f18c4cc8b998b1044f0980a5319d200206523
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-06 00:24:48 +0900

    [feature] 기본 웹 페이지 라우팅 추가 ( #3 )
    - HomeController에 홈페이지(`/`), 로그인 페이지(`/login`), 및 테스트 페이지(`/test`)에 대한 GET 요청 처리를 추가.
    
    - 각 경로는 적절한 프로토타입의 HTML 뷰를 반환하도록 설정.
    
    - 해당 뷰 목록들을 프로토타입으로 추후 개선 필요

---
+++ src/main/java/com/coupon/issuecouponservice/controller/user/HomeController.java
@@ -0,0 +1,27 @@
+package com.coupon.issuecouponservice.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class HomeController {
+
+    @GetMapping({"", "/"})
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
