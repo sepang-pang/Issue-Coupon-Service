804a25cbd725d3cbad543739bdf406fe840f0b0b
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-08 18:56:01 +0900

    [feature] 쿠폰 조회 시 쿠폰 이름 반환 추가 ( #10 )
    - 쿠폰을 조회할 때 쿠폰의 이름도 반환하여 확인하게 할 수 있도록 함.

--- src/main/java/com/coupon/issuecouponservice/dto/response/coupon/CouponForm.java
+++ src/main/java/com/coupon/issuecouponservice/dto/response/coupon/CouponForm.java
@@ -11,12 +11,14 @@ import java.time.LocalDateTime;
 @Getter
 public class CouponForm {
     private Long couponId;
+    private String couponName;
     private int totalQuantity;
     private int remainQuantity;
     private LocalDateTime expiredAt;
 
     public CouponForm(Coupon coupon) {
         this.couponId = coupon.getId();
+        this.couponName = coupon.getCouponName();
         this.totalQuantity = coupon.getTotalQuantity();
         this.remainQuantity = coupon.getRemainQuantity();
         this.expiredAt = coupon.getExpiredAt();
