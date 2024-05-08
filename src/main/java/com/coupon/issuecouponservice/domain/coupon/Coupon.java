e15575897628048e63f78991485f825af7f31580
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-08 11:22:13 +0900

    [feature] 쿠폰 엔티티 Timestamped 상속 및 만료일 필드 추가 ( #8 )
    - Coupon 엔티티에 만료일을 나타내는 'expired_at' 필드를 추가
    
    - 엔티티에 공통 시간 필드를 제공하는 Timestamped 클래스를 상속받도록 변경하여 생성 및 수정 시간을 자동으로 관리

--- src/main/java/com/coupon/issuecouponservice/domain/coupon/Coupon.java
+++ src/main/java/com/coupon/issuecouponservice/domain/coupon/Coupon.java
@@ -1,20 +1,20 @@
 package com.coupon.issuecouponservice.domain.coupon;
 
+import com.coupon.issuecouponservice.domain.common.Timestamped;
 import io.hypersistence.utils.hibernate.id.Tsid;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Table(name = "coupon")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Coupon {
+public class Coupon extends Timestamped {
 
     @Id
     @Tsid
@@ -32,10 +32,15 @@ public class Coupon {
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
 
+    @Column(name = "expired_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime expiredAt;
+
     @Builder
-    public Coupon(String couponName, int totalQuantity, int remainQuantity) {
+    public Coupon(String couponName, int totalQuantity, int remainQuantity, LocalDateTime expiredAt) {
         this.couponName = couponName;
         this.totalQuantity = totalQuantity;
         this.remainQuantity = remainQuantity;
+        this.expiredAt = expiredAt;
     }
 }
