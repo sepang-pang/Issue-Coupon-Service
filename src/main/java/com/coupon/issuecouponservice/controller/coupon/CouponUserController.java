554c670ff16e42e328206bba285f845eae39d074
ChuJH <09strawberry@naver.com>
2024-05-10 13:37:19 +0900

    [feature] @Secured 추가 ( #12 )
    - USER 권한만 가능

--- src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponUserController.java
+++ src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponUserController.java
@@ -17,6 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Secured("USER")
 public class CouponUserController {
 
     private final CouponService couponService;
