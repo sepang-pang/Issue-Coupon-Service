51f44432a33f6b01301661e358f6edc65d0758e0
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-05-17 12:46:50 +0900

    [refactor] 컨트롤러 어노테이션 수정 및 코드 정리 ( #18 )
    1. 불필요한 `@RequestMapping("/common")` 어노테이션 제거
       - URL 매핑을 더 명확하게 하기 위해 제거함.
    
    2. `@Secured("USER")`를 `@Secured(USER)`로 변경

--- src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponCommonController.java
+++ src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponCommonController.java
@@ -13,7 +13,6 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/common")
 public class CouponCommonController {
 
     private final CouponService couponService;
