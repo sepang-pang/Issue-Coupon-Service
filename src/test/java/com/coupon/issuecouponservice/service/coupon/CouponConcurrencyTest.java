5b286acb6163d88c60f2f0f4d8bb6dc559a17213
ChuJH <09strawberry@naver.com>
2024-05-27 08:31:14 +0900

    [refactor] 깃 액션 테스트 환경과 로컬 환경의 간극 줄이기 - 3 ( #33 ) - 테스트 코드 : 데이터 생성 및 삭제 로직 추가
    - 깃 테스트 환경에는 작성한 테스트 로직에 대입할 데이터가 없는 상황이다. 그렇기에 데이터를 직접 생성해주고 테스트 완료 시 데이터 삭제 로직 추가

--- src/test/java/com/coupon/issuecouponservice/service/coupon/CouponConcurrencyTest.java
+++ src/test/java/com/coupon/issuecouponservice/service/coupon/CouponConcurrencyTest.java
@@ -1,10 +1,14 @@
 package com.coupon.issuecouponservice.service.coupon;
 
+import com.coupon.issuecouponservice.domain.coupon.Coupon;
+import com.coupon.issuecouponservice.domain.user.Role;
 import com.coupon.issuecouponservice.domain.user.User;
 import com.coupon.issuecouponservice.dto.request.coupon.CouponIssueParam;
 import com.coupon.issuecouponservice.facade.RedissonLockFacade;
+import com.coupon.issuecouponservice.repository.coupon.CouponRepository;
 import com.coupon.issuecouponservice.repository.coupon.UserCouponRepository;
 import com.coupon.issuecouponservice.repository.user.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +16,8 @@ import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -32,6 +38,9 @@ class CouponConcurrencyTest {
     private RedissonLockFacade redissonLockFacade;
 
     @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
     private UserCouponRepository userCouponRepository;
 
     @Autowired
@@ -39,18 +48,42 @@ class CouponConcurrencyTest {
 
     CouponIssueParam param;
 
-    List<User> users;
+    Coupon coupon;
+
+    List<User> users = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-        param = new CouponIssueParam(1L);
+        for (int i = 0; i < 1000; i++ ) {
+            User user = User.builder()
+                    .username("test" + i)
+                    .nickName("Test" + i)
+                    .role(Role.USER)
+                    .email("test" + i + "@example.com")
+                    .provider("test")
+                    .providerId("test" + i)
+                    .build();
+            userRepository.save(user);
+            users.add(user);
+        }
 
-        users = userRepository.findAll();
+        coupon = Coupon.builder()
+                .couponName("테스트 쿠폰")
+                .couponImage("image")
+                .expiredAt(LocalDateTime.now().plusDays(30))
+                .totalQuantity(100)
+                .build();
+
+        couponRepository.save(coupon);
+
+        param = new CouponIssueParam(coupon.getId());
     }
 
     @AfterEach
     void after() {
         userCouponRepository.deleteAll();
+        couponRepository.deleteAll();
+        userRepository.deleteAll();
     }
 
     @Test
@@ -58,7 +91,7 @@ class CouponConcurrencyTest {
     @DisplayName("쿠폰 한 명 발급")
     void 쿠폰_한_명_발급() {
         // given
-        User user = userRepository.findById(1L).get();
+        User user = users.get(0);
 
         // when
         couponService.issueCoupon(param, user);
@@ -69,18 +102,18 @@ class CouponConcurrencyTest {
     }
 
     @Test
-    @DisplayName("쿠폰 여러 명 발급")
-    void 쿠폰_여러_명_발급() throws InterruptedException {
+    @DisplayName("쿠폰 여러 명 발급 - 레디슨")
+    void issueCoupon_redisson() throws InterruptedException {
         int threadCount = 1000;
         ExecutorService executorService = Executors.newFixedThreadPool(32);
         CountDownLatch latch = new CountDownLatch(threadCount);
 
         for (int i = 0; i < threadCount; i++) {
             final int threadNumber = i + 1;
-            int key = i;
+            User user = users.get(i);
             executorService.submit(() -> {
                 try {
-                    redissonLockFacade.issueCouponWithLock(param, users.get(key));
+                    redissonLockFacade.issueCouponWithLock(param, user);
                     System.out.println("Thread " + threadNumber + " - 성공");
 
                 } catch (PessimisticLockingFailureException e) {
@@ -100,7 +133,7 @@ class CouponConcurrencyTest {
 
         Long count = userCouponRepository.countByCouponId(param.getCouponId());
 
-        assertThat(count).isEqualTo(100);
+        Assertions.assertThat(count).isEqualTo(100);
     }
 
 
