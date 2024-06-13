3ddc01544a11554dad7da1fb0120d47eb6f5d10c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-14 00:00:37 +0900

    [feature] 프로필 설정 페이지 및 이미지 선택 기능 추가 ( #44 )
    - 이미지 선택 시 미리보기를 위한 JavaScript 기능 추가
    
    - CSS로 이미지 박스 스타일 설정 및 커서 반응 효과 추가
    
    - '건너뛰기' 버튼 및 '등록하기' 버튼 클릭 이벤트 처리
    
    - 서버에 프로필 정보를 PATCH 요청으로 전송하는 기능 구현

---
+++ src/main/resources/static/js/profile-setup.js
@@ -0,0 +1,46 @@
+function previewImage(input) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        document.getElementById('image-preview').src = e.target.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+}
+
+document.getElementById("skip").addEventListener("click", function () {
+    window.location.href = "/";
+})
+
+document.getElementById("submit").addEventListener("click", function () {
+    const formData = {
+        nickName: document.querySelector(".form-control").value,
+        image: ""
+    }
+
+    fetch("/user/profile/setup", {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    }).then(response => response.json())
+        .then(data => {
+            console.log("Success: ", data);
+            window.location.href = "/";
+        })
+        .catch((error) => {
+            console.error("Error: ", error);
+            alert("프로필 업데이트에 실패했습니다.");
+        });
+});
+
+
+// function collectFormDate() {
+//     const formData = new FormData();
+//     const image = document.getElementById("file-input").files[0];
+//     const nickName = document.querySelector(".form-control").value;
+//
+//     formData.append("file", image); // 파일 추가
+//     formData.append("param", new Blob([JSON.stringify({ nickName: nickName })], { type: "application/json" }));
+//
+//     return formData;
+// }
