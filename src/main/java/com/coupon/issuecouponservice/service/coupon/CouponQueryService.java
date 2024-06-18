1b2c4728251b81e24512b41df4443179a6ee82ad
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-18 12:53:48 +0900

    [feature] 쿠폰 스케줄링 작업의 트랜잭션 관리 적용 ( #47 )
    - CouponScheduler에 TransactionTemplate의 적용
    
    - 트랜잭션이 필요한 데이터베이스 작업을 위해 스케줄링된 작업 내에서 TransactionTemplate을 사용하여 트랜잭션을 구현
    
    - CouponQueryService 의 getCoupon 메서드 @Transactional(readOnly = true) 어노테이션 제거. 필요한 트랜잭션 처리는 호출되는 상위 서비스 레이어에서 관리.

--- src/main/java/com/coupon/issuecouponservice/service/coupon/CouponQueryService.java
+++ src/main/java/com/coupon/issuecouponservice/service/coupon/CouponQueryService.java
@@ -5,7 +5,6 @@ import com.coupon.issuecouponservice.domain.coupon.Coupon;
 import com.coupon.issuecouponservice.repository.coupon.CouponRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,14 +12,8 @@ public class CouponQueryService {
 
     private final CouponRepository couponRepository;
 
-    @Transactional(readOnly = true)
     public Coupon getCoupon(Long couponId) {
         return couponRepository.findOneCouponByCouponId(couponId)
                 .orElseThrow(() -> new IllegalArgumentException("유효하지 않는 쿠폰입니다."));
-    }
-
-    @Transactional
-    public void saveCoupon(Coupon coupon) {
-        couponRepository.save(coupon);
     }
 }
--- src/main/java/com/coupon/issuecouponservice/service/coupon/CouponScheduler.java
+++ src/main/java/com/coupon/issuecouponservice/service/coupon/CouponScheduler.java
@@ -5,34 +5,39 @@ import com.coupon.issuecouponservice.domain.coupon.CouponStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.ZoneId;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class CouponScheduler {
 
     private final CouponQueryService couponQueryService;
+    private final TransactionTemplate transactionTemplate;
     private final TaskScheduler taskScheduler;
 
     public void scheduleCouponStatusChange(Coupon coupon) {
         // 쿠폰 활성화 스케줄
-        taskScheduler.schedule(() -> activateCoupon(coupon.getId()), Date.from(coupon.getOpenAt().atZone(ZoneId.systemDefault()).toInstant()));
+        taskScheduler.schedule(() -> activateCoupon(coupon.getId()), coupon.getOpenAt().atZone(ZoneId.systemDefault()).toInstant());
 
         // 쿠폰 비활성화 스케줄
-        taskScheduler.schedule(() -> deactivateCoupon(coupon.getId()), Date.from(coupon.getClosedAt().atZone(ZoneId.systemDefault()).toInstant()));
+        taskScheduler.schedule(() -> deactivateCoupon(coupon.getId()), coupon.getClosedAt().atZone(ZoneId.systemDefault()).toInstant());
     }
 
     private void activateCoupon(Long couponId) {
-        Coupon coupon = couponQueryService.getCoupon(couponId);
-        coupon.updateCouponStatus(CouponStatus.ACTIVE);
-        couponQueryService.saveCoupon(coupon);
+        transactionTemplate.execute(status -> {
+            Coupon coupon = couponQueryService.getCoupon(couponId);
+            coupon.updateCouponStatus(CouponStatus.ACTIVE);
+            return null;
+        });
     }
 
     private void deactivateCoupon(Long couponId) {
-        Coupon coupon = couponQueryService.getCoupon(couponId);
-        coupon.updateCouponStatus(CouponStatus.CLOSED);
-        couponQueryService.saveCoupon(coupon);
+        transactionTemplate.execute(status -> {
+            Coupon coupon = couponQueryService.getCoupon(couponId);
+            coupon.updateCouponStatus(CouponStatus.CLOSED);
+            return null;
+        });
     }
 }
