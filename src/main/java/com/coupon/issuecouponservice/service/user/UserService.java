fa250e77d4f178227454754670fb22e16822cf21
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-20 21:09:12 +0900

    [feature] 사용자 프로필 수정 로직에서 세션 정보 갱신 추가 ( #49 )
    - 사용자 정보가 변경되었을 때, SecurityContext 내의 Authentication 객체를 갱신하여 세션 정보가 유효하도록 수정
    
    - UserDetailsImpl 클래스 내에 updateUser 메소드를 추가하여, 사용자 정보를 업데이트하는 로직을 구현
    
    - 프로필 정보 수정 후 "/my-page" 로 이동하도록 수정

--- src/main/java/com/coupon/issuecouponservice/service/user/UserService.java
+++ src/main/java/com/coupon/issuecouponservice/service/user/UserService.java
@@ -3,8 +3,12 @@ package com.coupon.issuecouponservice.service.user;
 import com.coupon.issuecouponservice.domain.user.User;
 import com.coupon.issuecouponservice.dto.request.user.UserModificationParam;
 import com.coupon.issuecouponservice.repository.user.UserRepository;
+import com.coupon.issuecouponservice.security.userdetails.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +25,23 @@ public class UserService {
         User findUser = getUser(user);
 
         findUser.modifyUserDetails(param);
+
+        // 현재 인증 객체 가져오기
+        Authentication currentAuth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) currentAuth.getPrincipal();
+
+        // UserDetailsImpl 내부의 User 정보 업데이트
+        userDetails.updateUser(findUser);
+
+        // 새로운 Authentication 객체 생성
+        Authentication newAuth = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                currentAuth.getCredentials(),
+                currentAuth.getAuthorities()
+        );
+
+        // SecurityContext에 업데이트된 Authentication 객체 재설정
+        SecurityContextHolder.getContext().setAuthentication(newAuth);
     }
 
     private User getUser(User user) {
