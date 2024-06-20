7ce05e3242dd506e4b05103a83b18a5ed995c800
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-20 18:06:50 +0900

    [feature] 마이페이지 컨트롤러 및 뷰 템플릿 업데이트 ( #49 )
    - 마이페이지 컨트롤러 사용자 정보 전달 로직 추가
    
    - Thymeleaf를 사용한 동적 사용자 닉네임 및 로그인 제공자 표시 기능 구현

--- src/main/java/com/coupon/issuecouponservice/controller/HomeController.java
+++ src/main/java/com/coupon/issuecouponservice/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.coupon.issuecouponservice.controller;
 
 import com.coupon.issuecouponservice.dto.response.coupon.CouponOneForm;
+import com.coupon.issuecouponservice.security.userdetails.UserDetailsImpl;
 import com.coupon.issuecouponservice.service.coupon.CouponService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,7 +40,8 @@ public class HomeController {
     }
 
     @GetMapping("/my-page")
-    public String myPage() {
+    public String myPage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("user", userDetails.getUser());
         return "my-page";
     }
 
