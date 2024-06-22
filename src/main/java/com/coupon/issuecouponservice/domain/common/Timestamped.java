e6d7943347165275bb20c85c050e169a7abca119
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-22 17:19:02 +0900

    [feature] @Getter 어노테이션을 Timestamped 클래스에 추가 ( #49 )
    - 모든 필드에 대한 getter 메서드 자동 생성으로 접근성 향상

--- src/main/java/com/coupon/issuecouponservice/domain/common/Timestamped.java
+++ src/main/java/com/coupon/issuecouponservice/domain/common/Timestamped.java
@@ -1,12 +1,14 @@
 package com.coupon.issuecouponservice.domain.common;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Timestamped {
