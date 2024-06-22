769b3f0dbc53e215c379916d052cb55c44b5c1e3
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-06-22 17:40:47 +0900

    [feature] UserCoupon 조회 메서드에 order by 추가 ( #49 )
    - order by uc.createdAt desc 를 설정함으로써, 본인 소유 쿠폰 조회 간 최근 발급 순서로 정렬

--- src/main/java/com/coupon/issuecouponservice/repository/coupon/UserCouponRepository.java
+++ src/main/java/com/coupon/issuecouponservice/repository/coupon/UserCouponRepository.java
@@ -11,7 +11,8 @@ public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
 
     @Query("select uc from UserCoupon uc " +
             "join fetch uc.coupon " +
-            "where uc.user.id = :userId")
+            "where uc.user.id = :userId " +
+            "order by uc.createdAt desc")
     List<UserCoupon> findUserCouponsByUserId(@Param("userId") Long userId);
 
     Long countByCouponId(Long couponId);
