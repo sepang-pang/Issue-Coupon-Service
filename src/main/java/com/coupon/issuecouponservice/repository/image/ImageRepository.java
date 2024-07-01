c9c0fa70fa70dc899dd0dea83b107668b29070b7
ChuJH <09strawberry@naver.com>
2024-07-01 18:14:18 +0900

    [refactor] Image 도메인 삭제 (#60)
    - Image 엔티티 삭제
    - Image 엔티티의 FileUrl 필드는 각각 Coupon 엔티티의 CouponImage 와 User 엔티티의 UserImage 로 대체

--- src/main/java/com/coupon/issuecouponservice/repository/image/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.coupon.issuecouponservice.repository.image;
-
-import com.coupon.issuecouponservice.domain.image.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-
-    Boolean existsByImageUrlAndId(String fileName, Long id);
-}
