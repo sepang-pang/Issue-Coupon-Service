c09d8a7e54b42b759da2311daef1e6f36876227b
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-07-02 21:26:43 +0900

    [feature] 불필요한 import 제거 ( #63 )
    - List 를 사용하지 않으므로 해당 import 제거

--- src/main/java/com/coupon/issuecouponservice/controller/HomeController.java
+++ src/main/java/com/coupon/issuecouponservice/controller/HomeController.java
@@ -16,8 +16,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 @RequiredArgsConstructor
 @Slf4j
