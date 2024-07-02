3bb56a0d6667c78538703c03d73cd5f816340f32
ChuJH <09strawberry@naver.com>
2024-07-02 11:18:10 +0900

    [hotfix] S3Config @Value 값 설정

--- src/main/java/com/coupon/issuecouponservice/config/S3Config.java
+++ src/main/java/com/coupon/issuecouponservice/config/S3Config.java
@@ -10,13 +10,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-    @Value("${ACCESS_KEY}")
+    @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("${SECRET_KEY}")
+    @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${REGION}")
+    @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
