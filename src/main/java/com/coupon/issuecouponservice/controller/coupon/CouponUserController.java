28d88179a3bcf3335d366f6a3feff6307efbf224
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-08-19 23:12:57 +0900

    [refactor] CouponUserController 리팩토링 ( #70 )
    - `CouponUserController` 클래스를 @Controller에서 @RestController로 변경하여 컨트롤러의 응답 방식을 단순화함.
    
    - 불필요한 `CouponService` 의존성 제거.
    
    - `@ResponseBody` 어노테이션 제거. `@RestController`로 인해 모든 메소드가 기본적으로 `@ResponseBody`를 포함하게 되어, 별도의 어노테이션 지정이 필요 없음.

--- src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponUserController.java
+++ src/main/java/com/coupon/issuecouponservice/controller/coupon/CouponUserController.java
@@ -2,41 +2,32 @@ package com.coupon.issuecouponservice.controller.coupon;
 
 import com.coupon.issuecouponservice.dto.request.coupon.CouponIssueParam;
 import com.coupon.issuecouponservice.dto.response.ApiResponseForm;
-import com.coupon.issuecouponservice.dto.response.coupon.CouponForm;
 import com.coupon.issuecouponservice.facade.RedissonLockFacade;
 import com.coupon.issuecouponservice.security.userdetails.UserDetailsImpl;
-import com.coupon.issuecouponservice.service.coupon.CouponService;
-import com.coupon.issuecouponservice.util.PaginationUtils;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.coupon.issuecouponservice.domain.user.Role.Authority.USER;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 @Secured(USER)
 public class CouponUserController {
 
-    private final CouponService couponService;
     private final RedissonLockFacade redissonLockFacade;
 
     // 쿠폰 발급
-    @ResponseBody
     @PostMapping("/coupon")
-    public ResponseEntity<ApiResponseForm> issueCoupon(@RequestBody CouponIssueParam couponIssueParam, @AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<ApiResponseForm> issueCoupon(@RequestBody CouponIssueParam couponIssueParam, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         redissonLockFacade.issueCouponWithLock(couponIssueParam, userDetails.getUser());
         return ResponseEntity.ok().body(new ApiResponseForm("쿠폰 발급에 성공했습니다.", HttpStatus.OK.value()));
     }
-
 }
