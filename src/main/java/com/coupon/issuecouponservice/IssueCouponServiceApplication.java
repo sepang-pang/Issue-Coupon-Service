0947d539481b0397a02cf73fe3073fa44ab3a7d5
ChuJH <09strawberry@naver.com>
2024-09-06 00:04:23 +0900

    [feature] Redis 를 활용한 세션 스토리지 구현 기능 추가 - 4
    - Redis 세션 사용 설정
    - `@EnableRedisHttpSession`는 Spring 에서 HTTP 세션을 Redis 에 저장하여 세션 관리를 분산 시스템에서 쉽게 할 수 있도록 해주는 어노테이션이다.

--- src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
+++ src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
@@ -4,11 +4,13 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableRedisHttpSession
 public class IssueCouponServiceApplication {
 
 	public static void main(String[] args) {
