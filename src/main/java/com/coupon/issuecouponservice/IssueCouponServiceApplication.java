3cd43e7808319af1851bce70db988f744513c606
오세창 <114675677+sepang-pang@users.noreply.github.com>
2024-09-10 18:55:45 +0900

    feature: redis config 설정

--- src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
+++ src/main/java/com/coupon/issuecouponservice/IssueCouponServiceApplication.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import java.util.TimeZone;
 
