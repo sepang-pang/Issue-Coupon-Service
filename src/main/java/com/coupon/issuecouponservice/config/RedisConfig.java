82f2d03b7ce5920558c838fff3a7ff17c26d1699
ChuJH <09strawberry@naver.com>
2024-09-12 15:16:27 +0900

    [refactor] 불필요한 코드 제거 및 인터페이스 추가
    - @EnableRedisHttpSession 어노테이션을 통해 세션 데이터를 redis 에 저장할 때 기본적인 JDK 직렬화를 사용한다. 그렇기에 RedisTemplate 을 정의할 필요가 없어졌기에 코드를 제거하였다.
    
    - Serializable 인터페이스는 메서드를 가지지 않는 마커 인터페이스이다. 객체의 직렬화 가능을 명시하는 역할을 하는데 객체의 직렬화를 위해서는 추가해줘야 한다.

--- src/main/java/com/coupon/issuecouponservice/config/RedisConfig.java
+++ src/main/java/com/coupon/issuecouponservice/config/RedisConfig.java
@@ -6,9 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -32,14 +30,4 @@ public class RedisConfig {
         return new LettuceConnectionFactory(configuration);
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate(){
-        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
-
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-
-        return redisTemplate;
-    }
 }
